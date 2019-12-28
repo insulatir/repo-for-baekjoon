206602ffee392ecb0f0c836d19b05da6fcf41f49
insulator <s42415466@gmail.com>
2019-12-28 15:52:08 +0900

    bronze 5

--- 16430 제리와 톰/src/Main.java
+++ 16430 제리와 톰/src/Main.java
@@ -1,9 +1,25 @@
-
+import java.util.*;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scan = new Scanner(System.in);
+		
+		int A = scan.nextInt();
+		int B = scan.nextInt();
+		
+		while ((B-A) % 2 == 0 && B % 2 == 0) {
+			A /= 2;
+			B /= 2;
+		}
+		
+		while ((B-A) % 3 == 0 && B % 3 == 0) {
+			A /= 3;
+			B /= 3;
+		}
+		
+		System.out.print(B-A + " " + B);
+		
+		scan.close();
 	}
 
 }
