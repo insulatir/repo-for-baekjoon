323fc5194d15847a4abd81615bd30a7afea3c850
insulator <s42415466@gmail.com>
2020-01-05 13:50:55 +0900

    bronze 4

---
+++ 15610 Abbey Courtyard/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		double a = scan.nextDouble();
+		System.out.print(Math.sqrt(a)*4);
+		
+		scan.close();
+	}
+
+}
