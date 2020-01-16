53bdeaf117eda089c3fce5547c35f9513c02af1e
insulator <s42415466@gmail.com>
2020-01-16 14:22:53 +0900

    silver 1

--- 02178 미로 탐색/src/Main.java
+++ 02178 미로 탐색/src/Main.java
@@ -1,64 +1,66 @@
-import java.io.*;
 import java.util.*;
 public class Main {
 	static int[][] map;
-	static boolean[][] visited;
-	static int[][] check;
+	static boolean[][] visit;
+	static int[][] count;
 	static int[] dx = {0, 0, 1, -1};
-	static int[] dy = {1, -1, 0, 0};
-	static int N, M;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static int[] dy = {-1, 1, 0, 0};
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
 		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
+		int N = scan.nextInt();
+		int M = scan.nextInt();
 		
 		map = new int[N][M];
-		visited = new boolean[N][M];
-		check = new int[N][M];
+		visit = new boolean[N][M];
+		count = new int[N][M];
 		
 		for (int i = 0; i < N; i++) {
-			String line = br.readLine();
+			String line = scan.next();
 			for (int j = 0; j < M; j++) {
-				map[i][j] = Integer.parseInt(line.charAt(j) + "");
+				map[i][j] = Integer.parseInt(line.substring(j, j+1));
 			}
 		}
+
+		bfs(0, 0, N, M);
 		
-		bfs(0, 0);
-		
-		bw.flush();
-		bw.close();
+		scan.close();
 	}
 
-	public static void bfs(int y, int x) {
-		Queue<Integer> q = new LinkedList<>();
+	public static class Location {
+		int y;
+		int x;
 		
-		visited[y][x] = true;
-		q.add(y * 1000 + x);
-		check[y][x] = 1;
+		public Location(int y, int x) {
+			this.y = y;
+			this.x = x;
+		}
+	}
+	
+	public static void bfs(int x, int y, int N, int M) {
+		Queue<Location> q = new LinkedList<>();
+		
+		visit[y][x] = true;
+		q.add(new Location(y, x));
+		count[y][x] = 1;
 		
 		while (!q.isEmpty()) {
-			int n = q.poll();
-			int ny = n / 1000;
-			int nx = n % 1000;
+			Location n = q.poll();
 			
-			if (ny == N-1 && nx == M-1) {
-				System.out.println(check[ny][nx]);
+			if (n.y == N - 1 && n.x == M - 1) {
+				System.out.print(count[n.y][n.x]);
 				break;
 			}
 			
 			for (int i = 0; i < 4; i++) {
-				int ndy = ny + dy[i];
-				int ndx = nx + dx[i];
-				if (0 <= ndy && ndy < N && 0 <= ndx && ndx < M) {
-					if (map[ndy][ndx] == 1 && visited[ndy][ndx] == false) {
-						// check에 기록되지 않은 것만 가능 (중복 방지)
-						visited[ndy][ndx] = true;
-						check[ndy][ndx] = check[ny][nx] + 1;
-						q.add(ndy * 1000 + ndx);
+				int nx = dx[i] + n.x;
+				int ny = dy[i] + n.y;
+				if (0 <= nx && nx < M && 0 <= ny && ny < N) {
+					if (map[ny][nx] == 1 && !visit[ny][nx]) {
+						visit[ny][nx] = true;
+						count[ny][nx] = count[n.y][n.x] + 1;
+						q.add(new Location(ny, nx));
 					}
 				}
 			}
