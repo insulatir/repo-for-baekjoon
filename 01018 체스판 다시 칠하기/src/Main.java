08556abe970667090604543e246d4598a686bddd
insulator <s42415466@gmail.com>
2020-01-29 14:36:37 +0900

    silver 5

--- 01018 체스판 다시 칠하기/src/Main.java
+++ 01018 체스판 다시 칠하기/src/Main.java
@@ -6,7 +6,9 @@ public class Main {
 
 		int N = scan.nextInt();
 		int M = scan.nextInt();
-		int correct = 0;
+		int c0 = 0;
+		int c1 = 0;
+		int min = Integer.MAX_VALUE;
 		int[][] board = new int[N][M];
 		for (int i = 0; i < N; i++) {
 			String line = scan.next();
@@ -20,6 +22,39 @@ public class Main {
 			}
 		}
 		
+		for (int k = 0; k <= N-8; k++) {
+			for (int l = 0; l <= M-8; l++) {
+				c0 = 0;
+				c1 = 0;
+				
+				for (int i = k; i < k+8; i++) {
+					for (int j = l; j < l+8; j++) {
+						int color = board[i][j];
+						
+						if ((i + j) % 2 == 0) {
+							if (color != 0) {
+								c0 += 1;
+							} else {
+								c1 += 1;
+							}
+						} else {
+							if (color != 1) {
+								c0 += 1;
+							} else {
+								c1 += 1;
+							}
+						}
+					}
+				}
+				
+				int c = (int) Math.min(c0, c1);
+				if (min > c) {
+					min = c;
+				}
+			}
+		}
+		
+		System.out.print(min);
 		
 		scan.close();
 	}
